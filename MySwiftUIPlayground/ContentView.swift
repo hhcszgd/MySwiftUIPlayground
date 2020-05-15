6665b17d1ded2b067600d614a254e6b69cc266d7
Connor Wang <hhcszgd@sina.com>
2020-05-15 11:17:02 +0800

    Initial Commit

---
+++ MySwiftUIPlayground/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MySwiftUIPlayground
+//
+//  Created by JohnConnor on 2020/5/15.
+//  Copyright © 2020 JohnConnor. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
